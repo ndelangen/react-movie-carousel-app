5e254576823cd270f79aa4dbf82c9283bbcfb3a0
Norbert de Langen <norbert@macaw.nl>
2016-04-27 01:49:03 +0200

    FIX issue with animation where card would be shown over when animating out

--- app/components/03-modules/carousel/main.js
+++ app/components/03-modules/carousel/main.js
@@ -4,21 +4,45 @@ import Card from '../../02-molecules/card/main';
 
 import styles from './main.css';
 
-const createItem = function (options, item, index, list) {
-	let stateClass;
-	const { activeIndex, path } = options;
 
-	// debugger;
+const getDisplayedItems = function (items, options) {
+	const { activeIndex } = options;
 
-	if (index === activeIndex) {
-		stateClass = styles['is-active'];
-	} else if (index === activeIndex - 1 || (index === list.length - 1 && activeIndex === 0)) {
-		stateClass = styles['is-prev'];
-	} else if (index === activeIndex + 1 || (index === 0 && activeIndex === list.length - 1)) {
-		stateClass = styles['is-next'];
-	} else {
-		stateClass = styles['is-hidden'];
+	const current = items[activeIndex];
+	const [prev2, prev1] = items.slice(0).splice(activeIndex - 2, 2).concat(items.slice(activeIndex - 1, 1));
+	const [next1, next2] = items.slice(0).splice(activeIndex + 1, 2).concat(items.slice(0, 2));
+
+	return {
+		prev2,
+		prev1,
+		current,
+		next1,
+		next2
 	}
+};
+
+const getItemStyle = function (activeItems, item) {
+	switch (item) {
+		case activeItems.prev2:
+			return styles['is-prev2'];
+		case activeItems.prev1:
+			return styles['is-prev1'];
+		case activeItems.current:
+			return styles['is-current'];
+		case activeItems.next1:
+			return styles['is-next1'];
+		case activeItems.next2:
+			return styles['is-next2'];
+		default:
+			return styles['hidden'];
+	};
+};
+
+const itemToComponent = function (options, item) {
+	let stateClass;
+	const { activeItems, path } = options;
+
+	stateClass = getItemStyle(activeItems, item);
 
 	return (
 		<li key={item.id} className={`${styles.item} ${stateClass}`}>
@@ -32,14 +56,20 @@ const createItem = function (options, item, index, list) {
 class Carousel extends Component {
 	render() {
 		const { items, path } = this.props;
-		const activeId = this.props.active
 
+		const activeId = this.props.active
 		const activeIndex = items.reduce((result, item, index) => { return item.id === activeId ? index : result }, 0);
+		const activeItems = getDisplayedItems(items, {activeIndex})
+
+		const options = Object.assign({}, this.props.options, {path, activeIndex, activeItems});
 
 		return (
 			<div className={styles.root}>
+				<div className={styles['tool-prev']} onClick={gotoItem.bind(undefined, items, options)}>PREV</div>
+				<div className={styles['tool-next']}>NEXT</div>
+
 				<ul className={styles.list}>
-					{items.map(createItem.bind(this, {path, activeIndex}))}
+					{items.map(itemToComponent.bind(undefined, options))}
 				</ul>
 			</div>
 		);
