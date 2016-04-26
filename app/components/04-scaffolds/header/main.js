354bddb7f9a091c355ca9e0087c35cfd6ae8933c
Norbert de Langen <norbert@macaw.nl>
2016-04-26 16:27:23 +0200

    ADD header scaffold

---
+++ app/components/04-scaffolds/header/main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import styles from './main.css';
+
+
+class Header extends Component {
+	render() {
+		return (
+			<header className={styles.root}>
+				<h1><span className={styles.title}>Universal React Movie-Carousel-App</span> <span className={styles.subtitle}>by Norbert de Langen</span></h1>
+			</header>
+		);
+	}
+}
+
+export default Header;
