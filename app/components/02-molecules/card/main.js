c527b8d363101a49ae8b66be93754084b421df2c
Norbert de Langen <norbert@macaw.nl>
2016-04-28 04:12:29 +0200

    ADD spiffy auto color functionality to Card, using a customized ColorThief and a subset of TinyColor CHANGED Card styling

--- app/components/02-molecules/card/main.js
+++ app/components/02-molecules/card/main.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Image from '../../01-atoms/image/main';
 
 import styles from './main.css';
 
 
 class Card extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			backgroundColor: 'hsl(0, 0%, 18%)',
+			textColor: 'hsl(0, 0%, 80%)'
+		};
+
+		this.imagePromise = new Promise((resolve, reject) => {
+			this.imageLoaded = (event) => {
+				const image = event.currentTarget;
+				image.crossOrigin = "Anonymous";
+				resolve(image);
+			};
+		}).then((existingImage) => {
+			return new Promise(function (resolve, reject) {
+				const image = document.createElement('img');
+				image.onload = (event) => {
+					resolve(image);
+				};
+				image.onerror = (event) => {
+					reject(image);
+				};
+				image.crossOrigin = "Anonymous";
+				image.src = existingImage.src;
+			});
+		});
+	}
+	componentDidMount() {
+		const instance = this;
+		const { item } = this.props;
+		const imgLoad = this.imagePromise;
+		const colorThiefLoad = System.import('../../00-utilities/color/color-thief.js');
+		const colorConverterLoad = System.import('../../00-utilities/color/color-converter.js');
+
+		Promise.all([imgLoad, colorThiefLoad, colorConverterLoad]).then((result) => {
+			const [image, colorThiefModule, colorConverterModule] = result;
+			const colorThiefInstance = new colorThiefModule.ColorThief();
+			const color = colorConverterModule.rgbToHsl(...colorThiefInstance.getColor(image));
+
+			instance.setState({
+				backgroundColor: `hsl(${color.h * 360}, ${color.s * 100}%, ${color.l * 100}%)`,
+				textColor: color.l < 0.4 ? `hsl(${color.h * 360}, ${color.s * 100}%, ${(color.l * 100) + 40}%)` : `hsl(${color.h * 360}, ${color.s * 100}%, ${(color.l * 100) - 40}%)`
+			});
+		});
+	}
 	render() {
+		const { state } = this;
 		const { item } = this.props;
 		return (
-			<div className={styles.root}>
-				{item.title}
-			</div>
+			<article className={styles.root}>
+				<div className={styles.content} style={{color: state.textColor}}>
+					<h1 className={styles.title}>{item.title}</h1>
+					<div className={styles.genre}>{item.genre}</div>
+					<div className={styles.rating}>IMDB score: {item.imdb}</div>
+					<div className={styles.group}>{item.group}</div>
+				</div>
+				<div className={styles.media}>
+					<Image src={item.img} ratio={`31/45`} onLoad={this.imageLoaded} />
+				</div>
+				<div className={styles.background} style={{backgroundColor: state.backgroundColor}}></div>
+			</article>
 		);
 	}
 }
