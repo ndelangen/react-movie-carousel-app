4046823ad94c9d6af3bb7eab3e5bff64ef059b5f
Norbert de Langen <norbert@macaw.nl>
2016-04-28 04:08:31 +0200

    ADD icon (caret / chevron) Atom

---
+++ app/components/01-atoms/icon/caret.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Right extends Component {
+	render() {
+		return (
+			<svg {...this.props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.1 30">
+				<path d="M11.4 15L.2 3.3C0 3 0 2.5.2 2.3l2-2c.3-.4.8-.4 1 0l13.7 14v1L3.2 29.8c-.3.3-.8.3-1 0l-2-2c-.3-.3-.3-.8 0-1l11-11.8z" />
+			</svg>
+		);
+	}
+};
