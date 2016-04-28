541e86a29759ae0427389afce4edb86e92f461e5
Norbert de Langen <norbert@macaw.nl>
2016-04-28 04:10:42 +0200

    FIX keyup binding not working well with a onKeyUp prop binding, use a document.addEventListener instead.

--- app/components/03-modules/carousel/main.js
+++ app/components/03-modules/carousel/main.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Link, browserHistory } from 'react-router';
 import Card from '../../02-molecules/card/main';
+import { Right as IconCaretLeft} from '../../01-atoms/icon/caret';
 
 import styles from './main.css';
 
-const gotoItem = function (items, options) {
-
-	browserHistory.push('/sample');
-};
+const keyHandler = function (event) {
+	const options = this.options;
+	switch (event.keyCode) {
+		case 39: //ArrowRight
+			browserHistory.push(`${options.path}/${options.activeItems.next1.id}`);
+			break;
+		case 37: //ArrowLeft
+			browserHistory.push(`${options.path}/${options.activeItems.prev1.id}`);
+			break;
+	}
+}
 
 const getDisplayedItems = function (items, options) {
 	const { activeIndex } = options;
@@ -50,14 +58,24 @@ const itemToComponent = function (options, item) {
 
 	return (
 		<li key={item.id} className={`${styles.item} ${stateClass}`}>
-			<Link to={`${path}/${item.id}`}>
-				<Card item={item}></Card>
-			</Link>
+			<Card item={item}></Card>
 		</li>
 	);
 };
 
+
 class Carousel extends Component {
+	componentDidMount() {
+		// save handler on instance for removal
+		this.keyHandler = keyHandler.bind(this);
+
+		// event listener has to be on document
+		// or else it won't trigger when component or children are out of focus
+		document.addEventListener('keyup', this.keyHandler, false);
+	}
+	componentDidUnMount() {
+		document.removeEventListener('keyup', this.keyHandler, false);
+	}
 	render() {
 		const { items, path } = this.props;
 
@@ -67,10 +85,13 @@ class Carousel extends Component {
 
 		const options = Object.assign({}, this.props.options, {path, activeIndex, activeItems});
 
+		// save options on instance for event-listener
+		this.options = options;
+
 		return (
 			<div className={styles.root}>
-				<div className={styles['tool-prev']} onClick={gotoItem.bind(undefined, items, options)}>PREV</div>
-				<div className={styles['tool-next']}>NEXT</div>
+				<Link className={styles['tool-next']} to={`${path}/${activeItems.next1.id}`}><IconCaretLeft className={styles['tool--icon']} /></Link>
+				<Link className={styles['tool-prev']} to={`${path}/${activeItems.prev1.id}`}><IconCaretLeft className={styles['tool--icon']} /></Link>
 
 				<ul className={styles.list}>
 					{items.map(itemToComponent.bind(undefined, options))}
